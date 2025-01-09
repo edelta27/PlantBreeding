c6df59b0b4e3c2372bb92ad896c22b129254a13f
Edyta <edysiaborowska27@gmail.com>
2025-01-09 20:22:19 +0100

    first commit - create project

---
+++ src/main/java/com/plantbreeding/PlantBreedingApplication.java
@@ -0,0 +1,13 @@
+package com.plantbreeding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantBreedingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantBreedingApplication.class, args);
+    }
+
+}

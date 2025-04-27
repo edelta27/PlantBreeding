ad860f95c8f4e2cb04095844e3f1cc0e33f1d207
Edyta <edysiaborowska27@gmail.com>
2025-04-27 17:30:51 +0200

    add interface and implement Task

--- src/main/java/com/plantbreeding/service/TaskService.java
+++ src/main/java/com/plantbreeding/service/TaskService.java
@@ -1,110 +1,16 @@
 package com.plantbreeding.service;
 
-import com.plantbreeding.repository.PlantRepository;
-import com.plantbreeding.repository.TaskRepository;
-import com.plantbreeding.domain.entity.Plant;
-import com.plantbreeding.domain.entity.Task;
 import com.plantbreeding.domain.enums.TaskStatus;
-import com.plantbreeding.exception.PlantNotFoundException;
-import com.plantbreeding.exception.TaskNotFoundException;
 import com.plantbreeding.dto.request.CreateTaskRequestDto;
 import com.plantbreeding.dto.request.TaskDto;
-import com.plantbreeding.mapper.TaskMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-@Log4j2
-@RequiredArgsConstructor
-public class TaskService {
-    private final TaskRepository taskRepository;
-    private final PlantRepository plantRepository;
-    private final TaskMapper taskMapper;
-
-    public List<TaskDto> findAll() {
-        log.info("retrieving all tasks: ");
-        List<Task> tasks = taskRepository.findAll();
-        return taskMapper.toDtoList(tasks);
-    }
-
-    public List<TaskDto> findTasksByDate(LocalDate taskDate) {
-        List<Task> tasks = taskRepository.findByTaskDate(taskDate);
-        List<TaskDto> taskDtos = taskMapper.toDtoList(tasks);
-        return taskDtos;
-    }
-
-    @Transactional
-    public void createTask(CreateTaskRequestDto request) {
-        Plant plant = plantRepository.findById(request.plantId())
-                .orElseThrow(() -> new PlantNotFoundException("Plant with id " + request.plantId() + " not found"));
-
-        LocalDate currentDate = request.startDate();
-        List<Task> tasks = new ArrayList<>();
-
-        while (!currentDate.isAfter(request.endDate())) {
-            Task task = new Task();
-            task.setPlant(plant);
-            task.setTaskType(request.taskType());
-            task.setTaskDate(currentDate);
-            task.setStatus(TaskStatus.SCHEDULED);
-
-            String note = "Recurring task scheduled on " + LocalDate.now();
-            task.setNotes(request.notes() != null ? request.notes() + " | " + note : note);
-            tasks.add(task);
-
-            switch (request.recurrence()) {
-                case DAILY -> currentDate = currentDate.plusDays(1);
-                case WEEKLY -> currentDate = currentDate.plusWeeks(1);
-                case MONTHLY -> currentDate = currentDate.plusMonths(1);
-            }
-        }
-
-        taskRepository.saveAll(tasks);
-    }
-
-    public List<TaskDto> findTasksByPlantId(Long plantId) {
-        return taskRepository.findByPlantId(plantId)
-                .stream()
-                .map(task -> new TaskDto(
-                        task.getId(),
-                        task.getTaskType(),
-                        task.getNotes(),
-                        task.getTaskDate(),
-                        task.getStatus(),
-                        task.getPlant().getId()
-                ))
-                .toList();
-    }
-
-    @Transactional
-    public void updateTasksStatus(Long id, TaskStatus taskStatus) {
-        Task task = taskRepository.findById(id)
-                .orElseThrow(() -> new TaskNotFoundException("Task with id " + id + " not found"));
-
-        if (taskStatus != null) {
-            task.setStatus(taskStatus);
-        }
-
-        taskRepository.save(task);
-    }
-
-    @Scheduled(cron = "0 0 0 * * ?")
-    @Transactional
-    public void markOverdueTasks() {
-        LocalDate today = LocalDate.now();
-        List<Task> overdueTasks = taskRepository.findByStatusAndTaskDateBefore(TaskStatus.SCHEDULED, today);
-
-        if (!overdueTasks.isEmpty()) {
-            overdueTasks.forEach(task -> task.setStatus(TaskStatus.OVERDUE));
-            taskRepository.saveAll(overdueTasks);
-            log.info("Updated {} tasks on OVERDUE", overdueTasks.size());
-        }
-    }
+public interface TaskService {
+    List<TaskDto> findAll();
+    List<TaskDto> findTasksByDate(LocalDate taskDate);
+    void createTask(CreateTaskRequestDto request);
+    List<TaskDto> findTasksByPlantId(Long plantId);
+    void updateTasksStatus(Long id, TaskStatus taskStatus);
+    void markOverdueTasks();
 }

faa866507f935abeef8a168263322c9e9e0c17d0
Edyta <edysiaborowska27@gmail.com>
2025-08-24 14:58:38 +0200

    test(integration): add shouldAddTaskSuccessfully and shouldUpdateTaskStatusSuccessfully

--- src/test/java/com/plantbreeding/controller/rest/TaskRestControllerIntegrationTest.java
+++ src/test/java/com/plantbreeding/controller/rest/TaskRestControllerIntegrationTest.java
@@ -1,5 +1,10 @@
 package com.plantbreeding.controller.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.plantbreeding.domain.enums.Recurrence;
+import com.plantbreeding.domain.enums.TaskStatus;
+import com.plantbreeding.domain.enums.TaskType;
+import com.plantbreeding.dto.request.CreateTaskRequestDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,6 +13,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDate;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -21,6 +28,9 @@ class TaskRestControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     void shouldReturnTaskDaily() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/tasks/daily?date=2025-03-01")
@@ -31,6 +41,45 @@ class TaskRestControllerIntegrationTest {
                 .andExpect(jsonPath("$[0].notes").value("Water me"))
                 .andExpect(jsonPath("$[0].taskDate").value("2025-03-01"))
                 .andExpect(jsonPath("$[0].status").value("OVERDUE"));
+    }
+
+    @Test
+    void shouldAddTaskSuccessfully() throws Exception {
+        // given
+        CreateTaskRequestDto requestDto = new CreateTaskRequestDto(
+                1L,
+                TaskType.WATERING,
+                "watering once a week",
+                TaskStatus.SCHEDULED,
+                LocalDate.of(2025, 7, 30),
+                LocalDate.of(2025, 10, 19),
+                Recurrence.WEEKLY
+        );
+
+        String json = objectMapper.writeValueAsString(requestDto);
+
+        // when + then
+        mockMvc.perform(MockMvcRequestBuilders.post("/tasks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Task(s) added successfully"))
+                .andExpect(jsonPath("$.status").value("OK"));
+    }
+
+    @Test
+    void shouldUpdateTaskStatusSuccessfully() throws Exception {
+        // given
+        Long taskId = 2L;
+        String newStatus = String.valueOf(TaskStatus.DONE);
+
+        // when + then
+        mockMvc.perform(MockMvcRequestBuilders.patch("/tasks/{id}", taskId)
+                        .param("taskStatus", newStatus)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Task updated successfully"))
+                .andExpect(jsonPath("$.status").value("OK"));
     }
 
 }
